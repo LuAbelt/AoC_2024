6ec3cbdaaa79c62beb50cd52916e93f4fac19e8c
LuAbelt <l.abelt@luabelt.de>
2024-12-02 12:38:11 +0100

    * Add clang format

--- src/day01/main.cpp
+++ src/day01/main.cpp
@@ -1,81 +1,76 @@
-#include <algorithm>
 #include "lib.h"
+#include <algorithm>
 
 using namespace std;
 
-pair<V<i64>,V<i64>> parseInputs() {
-    V<i64> input1, input2;
+pair<V<i64>, V<i64>> parseInputs() {
+  V<i64> input1, input2;
 
-    for(string line;getline(cin,line);)
-    {
-        stringstream ss(line);
+  for (string line; getline(cin, line);) {
+    stringstream ss(line);
 
-        i64 num1, num2;
+    i64 num1, num2;
 
-        ss >> num1 >> num2;
+    ss >> num1 >> num2;
 
-        input1.push_back(num1);
-        input2.push_back(num2);
-    }
+    input1.push_back(num1);
+    input2.push_back(num2);
+  }
 
-    return make_pair(input1,input2);
+  return make_pair(input1, input2);
 }
 
-void part1(){
-    auto input = parseInputs();
-    V<i64> list1 = input.first;
-    V<i64> list2 = input.second;
+void part1() {
+  auto input = parseInputs();
+  V<i64> list1 = input.first;
+  V<i64> list2 = input.second;
 
-    sort(list1.begin(),list1.end());
-    sort(list2.begin(),list2.end());
+  sort(list1.begin(), list1.end());
+  sort(list2.begin(), list2.end());
 
-    i64 res = 0;
+  i64 res = 0;
 
-    for(int i=0;i<list1.size();i++)
-    {
-        i64 distance = abs(list1[i] - list2[i]);
+  for (int i = 0; i < list1.size(); i++) {
+    i64 distance = abs(list1[i] - list2[i]);
 
-        res += distance;
-    }
+    res += distance;
+  }
 
-    cout << res << endl;
+  cout << res << endl;
 }
 
-void part2(){
-    auto input = parseInputs();
-    V<i64> list1 = input.first;
-    V<i64> list2 = input.second;
+void part2() {
+  auto input = parseInputs();
+  V<i64> list1 = input.first;
+  V<i64> list2 = input.second;
 
-    sort(list1.begin(),list1.end());
-    sort(list2.begin(),list2.end());
+  sort(list1.begin(), list1.end());
+  sort(list2.begin(), list2.end());
 
-    i64 res = 0;
+  i64 res = 0;
 
-    map<i64,i64> counts;
-    for(auto num : list2)
-    {
-        counts[num] += 1;
-    }
+  map<i64, i64> counts;
+  for (auto num : list2) {
+    counts[num] += 1;
+  }
 
-    for(auto num : list1)
-    {
-        res += counts[num]*num;
-    }
+  for (auto num : list1) {
+    res += counts[num] * num;
+  }
 
-    cout << res << endl;
+  cout << res << endl;
 }
 
-int main(int argc, char* argv[]) {
-    if(argc!=2)
-    {
-        std::cout << "Wrong number of arguments." << std::endl;
-        return 0;
-    }
-    if(std::string(argv[1])=="--one"){
-        part1();
-    }else if(std::string(argv[1])=="--two"){
-        part2();
-    } else{
-        std::cout << "Wrong usage!" << std::endl;
-    }
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "Wrong number of arguments." << std::endl;
+    return 0;
+  }
+  if (std::string(argv[1]) == "--one") {
+    part1();
+  } else if (std::string(argv[1]) == "--two") {
+    part2();
+  } else {
+    std::cout << "Wrong usage!" << std::endl;
+  }
 }
--- src/day02/main.cpp
+++ src/day02/main.cpp
@@ -1,194 +1,163 @@
-#include <algorithm>
 #include "lib.h"
+#include <algorithm>
 
 using namespace std;
-void part1(){
-    i64 safe = 0;
+void part1() {
+  i64 safe = 0;
 
-    for(string line;getline(cin,line);)
-    {
-        stringstream ss(line);
-        i64 current, last;
-        ss >> last;
-        ss >> current;
+  for (string line; getline(cin, line);) {
+    stringstream ss(line);
+    i64 current, last;
+    ss >> last;
+    ss >> current;
 
-        bool inc = false;
+    bool inc = false;
 
-        if(current==last)
-        {
-            continue;
-        }
-
-        if(current>last)
-        {
-            inc = true;
-        }
-
-        if(abs(last-current)<1 || abs(last-current)>3)
-        {
-            continue;
-        }
-
-        last = current;
-
-        bool isSafe = true;
-        while(ss>>current)
-        {
-            if(inc)
-            {
-                if(current<=last)
-                {
-                    isSafe = false;
-                    break;
-                }
-            } else
-            {
-                if(current>=last)
-                {
-                    isSafe = false;
-                    break;
-                }
-            }
-
-            if(abs(last-current)<1 || abs(last-current)>3)
-            {
-                isSafe = false;
-                break;
-            }
-
-            last = current;
-        }
-
-        if(isSafe)
-        {
-            ++safe;
-        }
+    if (current == last) {
+      continue;
     }
 
-    cout << safe << endl;
+    if (current > last) {
+      inc = true;
+    }
+
+    if (abs(last - current) < 1 || abs(last - current) > 3) {
+      continue;
+    }
+
+    last = current;
+
+    bool isSafe = true;
+    while (ss >> current) {
+      if (inc) {
+        if (current <= last) {
+          isSafe = false;
+          break;
+        }
+      } else {
+        if (current >= last) {
+          isSafe = false;
+          break;
+        }
+      }
+
+      if (abs(last - current) < 1 || abs(last - current) > 3) {
+        isSafe = false;
+        break;
+      }
+
+      last = current;
+    }
+
+    if (isSafe) {
+      ++safe;
+    }
+  }
+
+  cout << safe << endl;
 }
 
-bool checkSafeInc(i64 n1, i64 n2)
-{
-    if(n2<=n1)
-    {
-        return false;
-    }
+bool checkSafeInc(i64 n1, i64 n2) {
+  if (n2 <= n1) {
+    return false;
+  }
 
-    auto diff = abs(n1-n2);
-    if(diff<1 || diff>3)
-    {
-        return false;
-    }
+  auto diff = abs(n1 - n2);
+  if (diff < 1 || diff > 3) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
-bool checkSafeDec(i64 n1, i64 n2)
-{
-    if(n2>=n1)
-    {
-        return false;
-    }
+bool checkSafeDec(i64 n1, i64 n2) {
+  if (n2 >= n1) {
+    return false;
+  }
 
-    auto diff = abs(n1-n2);
-    if(diff<1 || diff>3)
-    {
-        return false;
-    }
+  auto diff = abs(n1 - n2);
+  if (diff < 1 || diff > 3) {
+    return false;
+  }
 
-    return true;
+  return true;
 }
 
-bool isSafeInc(V<i64> &nums)
-{
-    for(st idx=1;idx<nums.size();++idx)
-    {
-        if(nums[idx]<=nums[idx-1])
-        {
-            return false;
-        }
-
-        auto diff = abs(nums[idx]-nums[idx-1]);
-
-        if(diff<1 || diff>3)
-        {
-            return false;
-        }
+bool isSafeInc(V<i64> &nums) {
+  for (st idx = 1; idx < nums.size(); ++idx) {
+    if (nums[idx] <= nums[idx - 1]) {
+      return false;
     }
 
-    return true;
+    auto diff = abs(nums[idx] - nums[idx - 1]);
+
+    if (diff < 1 || diff > 3) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-bool isSafeDec(V<i64> &nums)
-{
-    for(st idx=1;idx<nums.size();++idx)
-    {
-        if(nums[idx]>=nums[idx-1])
-        {
-            return false;
-        }
-
-        auto diff = abs(nums[idx]-nums[idx-1]);
-
-        if(diff<1 || diff>3)
-        {
-            return false;
-        }
+bool isSafeDec(V<i64> &nums) {
+  for (st idx = 1; idx < nums.size(); ++idx) {
+    if (nums[idx] >= nums[idx - 1]) {
+      return false;
     }
 
-    return true;
+    auto diff = abs(nums[idx] - nums[idx - 1]);
+
+    if (diff < 1 || diff > 3) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-void part2(){
-    i64 safe = 0;
-    for(string line;getline(cin,line);)
-    {
-        stringstream ss(line);
+void part2() {
+  i64 safe = 0;
+  for (string line; getline(cin, line);) {
+    stringstream ss(line);
 
-        V<i64> numbers;
+    V<i64> numbers;
 
-        i64 num;
+    i64 num;
 
-        while(ss>>num)
-        {
-            numbers += num;
-        }
-
-        if(isSafeInc(numbers) || isSafeDec(numbers))
-        {
-            ++safe;
-            continue;
-        }
-
-        for(st idx = 0; idx< numbers.size();++idx)
-        {
-            V<i64> numbers2 = numbers;
-
-            numbers2.erase(std::next(numbers2.begin(),idx));
-
-            if(isSafeInc(numbers2) || isSafeDec(numbers2))
-            {
-                ++safe;
-                break;
-            }
-        }
+    while (ss >> num) {
+      numbers += num;
     }
 
-    cout << safe << endl;
+    if (isSafeInc(numbers) || isSafeDec(numbers)) {
+      ++safe;
+      continue;
+    }
+
+    for (st idx = 0; idx < numbers.size(); ++idx) {
+      V<i64> numbers2 = numbers;
+
+      numbers2.erase(std::next(numbers2.begin(), idx));
+
+      if (isSafeInc(numbers2) || isSafeDec(numbers2)) {
+        ++safe;
+        break;
+      }
+    }
+  }
+
+  cout << safe << endl;
 }
 
-int main(int argc, char* argv[]) {
-    if(argc!=2)
-    {
-        std::cout << "Wrong number of arguments." << std::endl;
-        return 0;
-    }
-    if(std::string(argv[1])=="--one"){
-        part1();
-    }else if(std::string(argv[1])=="--two"){
-        part2();
-    } else{
-        std::cout << "Wrong usage!" << std::endl;
-    }
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "Wrong number of arguments." << std::endl;
+    return 0;
+  }
+  if (std::string(argv[1]) == "--one") {
+    part1();
+  } else if (std::string(argv[1]) == "--two") {
+    part2();
+  } else {
+    std::cout << "Wrong usage!" << std::endl;
+  }
 }
