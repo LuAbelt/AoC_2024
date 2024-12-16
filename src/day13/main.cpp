3cf7f24b2dbcba3b780722343c9475f3978cd69a
LuAbelt <l.abelt@luabelt.de>
2024-12-16 15:36:22 +0100

    * Solutions day 13 and 14

--- src/day13/main.cpp
+++ src/day13/main.cpp
@@ -2,10 +2,164 @@
 #include "lib.h"
 
 using namespace std;
+using Coord2 = transform::Coord2;
+struct ClawMachine {
+  Coord2 AButton;
+  Coord2 BButton;
+  Coord2 PriceLocation;
+};
+
+auto parseInput() {
+  V<ClawMachine> Result;
+
+  std::regex ButtonRegx(".*X\\+(\\d+),.*Y\\+(\\d+)");
+  std::regex PrizeRegx(".*X=(\\d+),.*Y=(\\d+)");
+
+  for (string line1;getline(cin,line1);) {
+    if (line1.empty()) {
+      continue;
+    }
+    string line2,line3;
+    getline(cin,line2);
+    getline(cin,line3);
+
+    std::smatch matches;
+
+    regex_search(line1, matches, ButtonRegx);
+
+    i64 x,y;
+    stringstream ss;
+
+    ss = stringstream{matches[1].str()};
+    ss >> x;
+
+    ss = stringstream{matches[2].str()};
+    ss >> y;
+    Coord2 AButton{x,y};
+
+    regex_search(line2, matches, ButtonRegx);
+    ss = stringstream{matches[1].str()};
+    ss >> x;
+
+    ss = stringstream{matches[2].str()};
+    ss >> y;
+    Coord2 BButton{x,y};
+
+
+    regex_search(line3, matches, PrizeRegx);
+    ss = stringstream{matches[1].str()};
+    ss >> x;
+
+    ss = stringstream{matches[2].str()};
+    ss >> y;
+    Coord2 Target{x,y};
+
+    Result += ClawMachine{AButton, BButton, Target};
+
+  }
+  return Result;
+}
+
+void ctos(Coord2 C) {
+  cout << "(" << C.x() << "|" << C.y() << ")";
+}
+
+i64 getMinCost(ClawMachine &Machine) {
+  i64 Min = maxval<i64>();
+
+  Coord2 Remaining = Machine.PriceLocation;
+  i64 CurrentCost = 0;
+
+  while (Remaining.x() > 0
+    && Remaining.y() > 0) {
+    if ((Remaining.x() % Machine.BButton.x()) == 0
+       && (Machine.BButton.y() * (Remaining.x()/Machine.BButton.x())) == Remaining.y()) {
+      Min = min(Min, CurrentCost + (Remaining.x()/Machine.BButton.x()));
+    }
+
+    Remaining -= Machine.AButton;
+    CurrentCost += 3;
+  }
+
+  return Min == maxval<i64>()?0:Min;
+}
+
+i64 getMinCostFast(ClawMachine &Machine) {
+  auto &AButton = Machine.AButton;
+  auto &BButton = Machine.BButton;
+  auto &PriceLoc = Machine.PriceLocation;
+  // Case 1: Check if A/B are linearly dependent
+  if (
+    AButton.x() * BButton.y() == AButton.y() * BButton.x()
+  ) {
+    i64 ACost = maxval<i64>();
+    i64 BCost = maxval<i64>();
+
+    if (PriceLoc.x() % AButton.x() == 0
+      && PriceLoc.y() == AButton.y() * (PriceLoc.x()/AButton.x())) {
+      ACost = 3 *PriceLoc.x()/AButton.x();
+    }
+
+    if (PriceLoc.x() % BButton.x() == 0
+      && PriceLoc.y() == BButton.y() * (PriceLoc.x()/BButton.x())) {
+      BCost = PriceLoc.x()/BButton.x();
+    }
+
+    if (ACost != maxval<i64>() || BCost != maxval<i64>()) {
+      return min(ACost,BCost);
+    }
+
+    return 0;
+  }
+
+  // Other case: Solve linear equation system
+  i64 xa = AButton.x();
+  i64 ya = AButton.y();
+  i64 xb = BButton.x();
+  i64 yb = BButton.y();
+  i64 xp = PriceLoc.x();
+  i64 yp = PriceLoc.y();
+
+  i64 det = (xa*yb) - (ya*xb);
+  i64 det1 = (xp*yb) - (yp*xb);
+  i64 det2 = (xa*yp) - (ya*xp);
+
+  i64 a = det1/det;
+  i64 b = det2/det;
+
+  if ((AButton*a) + (BButton*b) == PriceLoc) {
+    return 3*a+b;
+  }
+  return 0;
+}
+
+
+
 void part1(){
+  auto Machines = parseInput();
+
+  i64 Res = 0;
+   for (auto& Machine : Machines) {
+     Res += getMinCost(Machine);
+   }
+
+  cout << Res << endl;
 }
 
 void part2(){
+  auto Machines = parseInput();
+
+  for (auto &Machine : Machines) {
+    Machine.PriceLocation += Coord2{10000000000000,10000000000000};
+  }
+
+  i64 Res = 0;
+  for (auto& Machine : Machines) {
+    i64 Cost = getMinCostFast(Machine);
+    Res += Cost;
+  }
+
+  cout << Res << endl;
 }
 
 int main(int argc, char* argv[]) {
--- src/day14/main.cpp
+++ src/day14/main.cpp
@@ -2,10 +2,155 @@
 #include "lib.h"
 
 using namespace std;
-void part1(){
+using Coord2 = transform::Coord2;
+
+struct Robot {
+  Coord2 StartPos;
+  Coord2 Velocity;
+};
+void ctos(Coord2 C) {
+  cout << "(" << C.x() << "|" << C.y() << ")";
 }
 
-void part2(){
+auto parseInput() {
+  V<Robot> Result;
+  for (string line;getline(cin,line);) {
+    auto parts = util::split(line);
+
+    auto coordS = util::split(parts[0],"=");
+    auto Vals = util::fromString<i64>(util::split(coordS[1],","));
+
+    Coord2 Start{Vals[0],Vals[1]};
+
+    coordS = util::split(parts[1],"=");
+    Vals = util::fromString<i64>(util::split(coordS[1],","));
+
+    Coord2 Velo{Vals[0],Vals[1]};
+
+    Result += Robot{Start,Velo};
+  }
+
+  return Result;
+}
+
+void part1() {
+  auto Robots = parseInput();
+
+  Coord2 Dimensions{101,103};
+  V<Coord2> EndPositions;
+
+  for (auto &Robot: Robots) {
+    while (Robot.Velocity.x()<0) {
+      Robot.Velocity.x() += Dimensions.x();
+    }
+
+    while (Robot.Velocity.y()<0) {
+      Robot.Velocity.y() += Dimensions.y();
+    }
+
+    Coord2 EndPos = Robot.StartPos + Robot.Velocity*100;
+    EndPos.x() %= Dimensions.x();
+    EndPos.y() %= Dimensions.y();
+
+    EndPositions += EndPos;
+  }
+
+  array<i64,4> Quadrants{0,0,0,0};
+
+  for (auto &EndPos : EndPositions) {
+    if (EndPos.x() == (Dimensions.x()/2) || EndPos.y() == (Dimensions.y()/2)) {
+      continue;
+    }
+
+    //ctos(EndPos); cout << "//";
+
+    auto idx = 0;
+    if (EndPos.x() > (Dimensions.x()/2)) {
+      idx += 1;
+    }
+
+    if (EndPos.y() > (Dimensions.y()/2)) {
+      idx +=2;
+    }
+
+    //cout << idx << endl;
+    Quadrants[idx]++;
+  }
+
+  cout << Quadrants[0]*Quadrants[1]*Quadrants[2]*Quadrants[3] << endl;
+}
+
+void print(V<Robot> &Robots) {
+  V<string> Output(103, string(101,'.'));
+
+  for (auto &Robot : Robots) {
+    Output[Robot.StartPos.y()][Robot.StartPos.x()] = '#';
+  }
+
+  for (auto &S : Output) {
+    cout << S << endl;
+  }
+}
+
+bool treeLike(V<Robot> &Robots) {
+  V<V<bool>> Output(103,V<bool>(101, false));
+
+  for (auto &Robot : Robots) {
+    Output[Robot.StartPos.y()][Robot.StartPos.x()] = true;
+  }
+
+  auto Dirs  = {Coord2{1,0},Coord2{1,1},Coord2{0,1},
+    Coord2{-1,1},Coord2{-1,0},Coord2{-1,-1}
+    ,Coord2{0,-1},Coord2{1,-1}};
+
+  i64 Result = 0;
+
+  for (i64 x = 1; x<100; x++) {
+    for (i64 y = 1; y<102; ++y) {
+      i64 nCnt = 0;
+
+      for (auto& Dir : Dirs) {
+        Coord2 Pos{x+Dir.x(),y+Dir.y()};
+        if (Output[Pos.y()][Pos.x()]) {
+          ++nCnt;
+        }
+      }
+
+      if (nCnt==8) {
+        ++Result;
+      }
+    }
+  }
+
+  return Result > 30;
+}
+
+void part2() {
+  auto Robots = parseInput();
+
+  for (auto &Robot: Robots) {
+    while (Robot.Velocity.x()<0) {
+      Robot.Velocity.x() += 101;
+    }
+
+    while (Robot.Velocity.y()<0) {
+      Robot.Velocity.y() += 103;
+    }
+  }
+
+  for (st Seconds = 0;Seconds<101*103;++Seconds) {
+    if (treeLike(Robots)) {
+      cout << "Second " << Seconds << endl;
+      print(Robots);
+    }
+
+    for (auto& Robot : Robots) {
+      Robot.StartPos += Robot.Velocity;
+
+      Robot.StartPos.x() %= 101;
+      Robot.StartPos.y() %= 103;
+    }
+  }
 }
 
 int main(int argc, char* argv[]) {
