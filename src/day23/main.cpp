44ebb08fea09d5ccb5ac95161e7972dee04bd422
LuAbelt <l.abelt@luabelt.de>
2024-12-24 02:19:23 +0100

    * Solutions day 23 (Slow)

--- src/day23/main.cpp
+++ src/day23/main.cpp
@@ -2,10 +2,122 @@
 #include "lib.h"
 
 using namespace std;
-void part1(){
+
+auto parseInput() {
+  set<string> Nodes;
+  map<string, set<string>> Neighbours;
+  for (auto &Line : IO::fromCin()) {
+    auto Parts = util::split(Line, "-");
+    string Left = Parts[0];
+    string Right = Parts[1];
+
+    Nodes += Left;
+    Nodes += Right;
+    Neighbours[Left] += Right;
+    Neighbours[Right] += Left;
+  }
+
+  return make_tuple(Nodes, Neighbours);
 }
 
-void part2(){
+void part1(){
+  auto [Nodes, Neighbours] = parseInput();
+
+  set<set<string>> Result;
+  for (const string &Node1 : Nodes) {
+    for (const string &Node2 : Neighbours[Node1]) {
+      for (const string &Node3 : Neighbours[Node2]) {
+        if (Node3 == Node1) {
+          continue;
+        }
+
+        if (!Neighbours[Node1].contains(Node3)) {
+          continue;
+        }
+
+        if (!(Node1.starts_with('t')||Node2.starts_with('t') || Node3.starts_with('t'))) {
+          continue;
+        }
+
+        Result += set{Node1,Node2,Node3};
+      }
+    }
+  }
+
+  cout << Result.size() << endl;
+}
+
+set<string> findMaxClique(const string &StartNode, map<string, set<string>> &Neighbours) {
+  set<string> Result{StartNode};
+
+  set<set<string>> Candidates;
+  map<set<string>, bool> Added;
+
+  for (auto& Neighbour: Neighbours[StartNode]) {
+    Candidates += set{StartNode, Neighbour};
+    Added[set{StartNode, Neighbour}] = true;
+  }
+
+
+
+  while (!Candidates.empty()) {
+    set<set<string>> NewCandidates;
+
+    for (auto& Candidate : Candidates){
+      Result = Candidate;
+      set<string> CommonNeighbours = Neighbours[*Result.begin()];
+
+      for (auto& Node : Result) {
+        V<string> Intersection;
+        ranges::set_intersection(CommonNeighbours, Neighbours[Node],
+          back_inserter(Intersection));
+
+        CommonNeighbours = set(Intersection.begin(),Intersection.end());
+      }
+
+      for (auto &NewNode : CommonNeighbours) {
+        if (Result.contains(NewNode)) {
+          continue;
+        }
+
+        set<string> NewCandidate = Result;
+        NewCandidate += NewNode;
+        if (Added[NewCandidate]) {
+          continue;
+        }
+
+        Added[NewCandidate] = true;
+
+        NewCandidates.emplace(NewCandidate);
+      }
+    }
+
+    Candidates = NewCandidates;
+  }
+
+  return Result;
+}
+
+void part2() {
+  auto [Nodes, Neighbours] = parseInput();
+
+  cout << "Num total nodes: " << Nodes.size() << endl;
+
+  set<string> CurMaxClique{};
+  i64 N = 0;
+
+  for (auto &Node : Nodes) {
+    cout << "Checking Node Number " << N++ << endl;
+    auto NewClique = findMaxClique(Node,Neighbours);
+
+    if (NewClique.size()>CurMaxClique.size()) {
+      CurMaxClique = NewClique;
+    }
+  }
+
+  for (auto &Node : CurMaxClique) {
+    cout << Node << ",";
+  }
 }
 
 int main(int argc, char* argv[]) {
