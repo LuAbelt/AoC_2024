3ccc9e698f321283f352a6cbcf5b47379272eb09
LuAbelt <l.abelt@luabelt.de>
2024-12-26 01:41:23 +0100

    * Solutions day 24 & 25

--- src/day24/main.cpp
+++ src/day24/main.cpp
@@ -1,11 +1,210 @@
-#include <algorithm>
 #include "lib.h"
 
 using namespace std;
-void part1(){
+struct Gate {
+  virtual ~Gate() = default;
+  virtual bool evaluate() = 0;
+};
+
+map<string, std::unique_ptr<Gate>> Producers;
+
+struct True : public Gate {
+  bool evaluate() override {
+    return true;
+  };
+};
+
+struct False : public Gate {
+  bool evaluate() override {
+    return false;
+  }
+};
+
+struct Logical : public Gate {
+  enum class Type {
+    OR,
+    AND,
+    XOR
+  } GType;
+
+  string LHS;
+  string RHS;
+
+  Logical(Type T, string lhs, string rhs) : GType(T), LHS(lhs), RHS(rhs){};
+
+  bool evaluate() override {
+    if (!Value.has_value()) {
+      switch (GType) {
+      case Type::OR:
+        Value = Producers[LHS]->evaluate() || Producers[RHS]->evaluate();
+        break;
+      case Type::XOR:
+        Value = Producers[LHS]->evaluate() ^ Producers[RHS]->evaluate();
+        break;
+      case Type::AND:
+        Value = Producers[LHS]->evaluate() && Producers[RHS]->evaluate();
+        break;
+      }
+    }
+
+    return Value.value();
+  }
+private:
+  optional<bool> Value;
+};
+
+auto parseInputs() {
+  for (string Line;getline(cin,Line);) {
+    if (Line.empty()) {
+      break;
+    }
+
+    string NodeName = Line.substr(0,3);
+
+    if (Line[Line.size()-1]=='0') {
+      Producers[NodeName] = make_unique<False>();
+    } else {
+      Producers[NodeName] = make_unique<True>();
+    }
+  }
+
+  set<string> OutRegisters;
+
+  for (string Line;getline(cin,Line);) {
+    auto Parts = util::split(Line, "->");
+
+    string NodeName = Parts[1];
+
+    if (NodeName.starts_with("z")) {
+      OutRegisters += NodeName;
+    }
+
+    string SplitBy;
+    Logical::Type LType;
+    if (Parts[0].contains("AND")) {
+      SplitBy = "AND";
+      LType = Logical::Type::AND;
+    }
+
+    if (Parts[0].contains("OR")) {
+      SplitBy = "OR";
+      LType = Logical::Type::OR;
+    }
+
+    if (Parts[0].contains("XOR")) {
+      SplitBy = "XOR";
+      LType = Logical::Type::XOR;
+    }
+
+    auto Operators  = util::split(Parts[0], SplitBy);
+
+    Producers[NodeName] = make_unique<Logical>(LType, Operators[0], Operators[1]);
+  }
+
+  return OutRegisters;
 }
 
-void part2(){
+void part1() {
+  auto OutRegisters  = parseInputs();
+
+  i64 curPos = 0;
+  i64 Res = 0;
+  for (auto& R : OutRegisters) {
+    Res += (i64(Producers[R]->evaluate()) << curPos);
+    ++curPos;
+  }
+
+  cout << Res << endl;
+}
+
+void part2() {
+  auto OutRegisters = parseInputs();
+
+  map<string, set<string>> Participating;
+  for (auto &[Name, Gate] : Producers) {
+    if (auto LGate = dynamic_cast<Logical*>(Gate.get())) {
+      Participating[LGate->LHS] += Name;
+      Participating[LGate->RHS] += Name;
+    }
+  }
+
+  set<string> WrongRegisters;
+
+  // First check that all z Gates are wired correctly:
+  for (i64 Bit = 0; Bit<45;++Bit) {
+    string RegName = "z";
+    RegName += ((Bit<10)?"0":"") + to_string(Bit);
+
+    if (auto LGate = dynamic_cast<Logical*>(Producers[RegName].get())) {
+      if (LGate->GType == Logical::Type::XOR) {
+        continue;
+      }
+
+      WrongRegisters += RegName;
+    } else {
+      WrongRegisters += RegName;
+    }
+  }
+
+  // Try to verify correct result bit
+  for (i64 Bit = 1; Bit<45;++Bit) {
+    string XRegName = "x";
+    XRegName += ((Bit<10)?"0":"") + to_string(Bit);
+    string YRegName = XRegName;
+    YRegName[0] = 'y';
+    string ZRegName = XRegName;
+    ZRegName[0] = 'z';
+
+    // Identify first XOR Gate
+    string XORReg;
+    for (auto &Name : Participating[XRegName]) {
+      if (auto LGate = dynamic_cast<Logical*>(Producers[Name].get())) {
+        if (LGate->GType != Logical::Type::XOR) {
+          continue;
+        }
+
+        if (LGate->RHS != YRegName && LGate->LHS != YRegName) {
+          continue;
+        }
+
+        XORReg = Name;
+        break;
+      }
+    }
+
+    if (auto LGate = dynamic_cast<Logical*>(Producers[ZRegName].get())) {
+      if (LGate->RHS == XORReg || LGate->LHS == XORReg) {
+        continue;
+      }
+
+      WrongRegisters += ZRegName;
+    }
+  }
+
+  // Try to verify the carry registers
+  V<string> CarryRegisters(45,"");
+  for (auto Gate : Participating["x00"]) {
+    if (auto LGate = dynamic_cast<Logical*>(Producers[Gate].get())) {
+      if (LGate->GType != Logical::Type::AND) {
+        continue;
+      }
+
+      CarryRegisters[0] = Gate;
+    }
+  }
+
+  for (i64 Bit = 1; Bit<45;++Bit) {
+    string XRegName = "x";
+    XRegName += ((Bit<10)?"0":"") + to_string(Bit);
+    string YRegName = XRegName;
+    YRegName[0] = 'y';
+
+    string LastCarry = CarryRegisters[Bit-1];
+  }
+
+  for (auto &Name : WrongRegisters) {
+    cout << Name << ",";
+  }
 }
 
 int main(int argc, char* argv[]) {
--- src/day25/main.cpp
+++ src/day25/main.cpp
@@ -2,10 +2,68 @@
 #include "lib.h"
 
 using namespace std;
-void part1(){
+
+auto parseInputs() {
+  V<array<i64, 5>> Keys;
+  V<array<i64, 5>> Locks;
+  for (string Line; getline(cin,Line);) {
+    if (Line.empty()) {
+      continue;
+    }
+
+    bool isLock = Line.starts_with("#");
+    array<i64, 5> Schematic{0,0,0,0,0};
+
+    for (st Pos = 0; Pos < 5; ++Pos) {
+      if (Line[Pos] == '#') {
+        Schematic[Pos]++;
+      }
+    }
+
+    for (st i = 0 ; i<6;++i) {
+      getline(cin, Line);
+
+      for (st Pos = 0; Pos < 5; ++Pos) {
+        if (Line[Pos] == '#') {
+          Schematic[Pos]++;
+        }
+      }
+    }
+
+    if (isLock) {
+      Locks += Schematic;
+    } else {
+      Keys += Schematic;
+    }
+  }
+
+  return make_tuple(Keys, Locks);
 }
 
-void part2(){
+void part1() {
+  auto [Keys, Locks] = parseInputs();
+
+  i64 Res = 0;
+  for (auto &Lock : Locks) {
+    for (auto &Key : Keys) {
+      bool Fit = true;
+      for (st Pos = 0; Pos<5; ++Pos) {
+        if (Lock[Pos] + Key[Pos] > 7) {
+          Fit = false;
+          break;
+        }
+      }
+
+      if (Fit) {
+        ++Res;
+      }
+    }
+  }
+
+  cout << Res << endl;
+}
+
+void part2() {
 }
 
 int main(int argc, char* argv[]) {
